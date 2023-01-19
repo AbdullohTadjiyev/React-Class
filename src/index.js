b85c13a149876c83db23196600df15e6917e7814
Abdullo <abdullotadjiyev16@gmail.com>
2023-01-19 11:57:34 +0500

    React Class

--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+class Users{
+    name = "" 
+    surename = ""
+    age = ""
+    is_married = ""
+    work = ""
+    country = {
+        home: "",
+        zip: "",
+        number: "",
+    }
+    skills = []
+ 
+    constructor(name, surename, age , is_married, work, country_home, country_zip , country_number , ...skills){
+        this.name = name
+        this.surename = surename
+        this.age = age
+        this.is_married = is_married
+        this.work =work
+        this.country.home = country_home
+        this.country.zip = country_zip
+        this.country.number =country_number
+        this.skills  = skills
+    }
+}
+let user1 = new Users("Abdulloh","Tadjiyev","17","No","web Developer","Konimex65","#57675676","7711", "HTML","Css","JS script","React", "Python" )
+let user2 = new Users("Jony" ,"Tom", "23","Yes","Dispatcher", "New Jersy","#465465","4545","Dispatcher", "painter", "Gardener")
+let user3 = new Users("Jony" ,"Tom", "23","Yes","Dispatcher", "New Jersy","#465465","4545","Dispatcher", "painter", "Gardener")
+let user4 = new Users("Jony" ,"Tom", "23","Yes","Dispatcher", "New Jersy","#465465","4545","Dispatcher", "painter", "Gardener")
+console.log(user1 , user2 , user3 , user4);
